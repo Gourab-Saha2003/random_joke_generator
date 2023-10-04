3526430ab2030a7606ea09bb1f378ec0e249a9b9
Gourab Saha <gourabsaha2062003@gmail.com>
2023-10-05 00:48:17 +0530

    create file

---
+++ script.js
@@ -0,0 +1,16 @@
+const joke=document.querySelector(".joke");
+const btn=document.querySelector(".btn");
+const url="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+let getjoke=()=>{
+    joke.classList.remove("fade");
+
+    fetch(url)
+    .then(data=> data.json())
+    .then(item=>{
+        joke.innerHTML=`${item.joke}`;
+        joke.classList.add("fade");
+    });
+}
+
+btn.addEventListener("click",getjoke);
+getjoke();
