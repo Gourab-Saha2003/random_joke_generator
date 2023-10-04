a8e4034ffca3e5c3dc29d6615f4d2304a42ef16c
Gourab Saha <gourabsaha2062003@gmail.com>
2023-10-05 00:50:16 +0530

    format document

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const joke=document.querySelector(".joke");
-const btn=document.querySelector(".btn");
-const url="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
-let getjoke=()=>{
-    joke.classList.remove("fade");
+const joke = document.querySelector(".joke");
+const btn = document.querySelector(".btn");
+const url =
+  "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+let getjoke = () => {
+  joke.classList.remove("fade");
 
-    fetch(url)
-    .then(data=> data.json())
-    .then(item=>{
-        joke.innerHTML=`${item.joke}`;
-        joke.classList.add("fade");
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      joke.innerHTML = `${item.joke}`;
+      joke.classList.add("fade");
     });
-}
+};
 
-btn.addEventListener("click",getjoke);
+btn.addEventListener("click", getjoke);
 getjoke();
